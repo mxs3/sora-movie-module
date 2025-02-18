6020a17ba81450441a7df5446800efa53a8e1b65
Ibrahim Sulejmenov <ibrahim@Ibrahim--MacBook-Air.local>
2025-02-18 17:01:04 +0100

    adding captions to bingeflex: fix 78

--- bingeflex/bingeflex.js
+++ bingeflex/bingeflex.js
@@ -194,7 +194,9 @@ async function extractStreamUrl(url) {
                     const apiUrl = `https://rivestream.live/api/backendfetch?requestID=movieVideoProvider&id=${movieId}&service=${service}&secretKey=${secretKey[j]}&proxyMode=noProxy`;
                     
                     try {
-                        const data = await safeJsonFetch(apiUrl);
+                        const responseText = await fetch(apiUrl);
+                        const data = JSON.parse(responseText);
+
                         if (data) {
                             const hlsSource = data.data?.sources?.find(source => source.format === 'hls');
                             const subtitleTrack = data.data?.captions?.find(track =>
@@ -250,7 +252,8 @@ async function extractStreamUrl(url) {
                     const apiUrl = `https://rivestream.live/api/backendfetch?requestID=tvVideoProvider&id=${showId}&season=${seasonNumber}&episode=${episodeNumber}&service=${service}&secretKey=${secretKey[j]}&proxyMode=noProxy`;
                     
                     try {
-                        const data = await safeJsonFetch(apiUrl);
+                        const responseText = await fetch(apiUrl);
+                        const data = JSON.parse(responseText);
                         
                         if (data) {
                             const hlsSource = data.data?.sources?.find(source => source.format === 'hls');
