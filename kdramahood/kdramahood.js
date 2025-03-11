efea6563cff9ce3b1d268f7fcd276731b3d919c5
Ibrahim Sulejmenov <batonchik2004@gmail.com>
2025-03-11 21:11:26 +0100

    added kdramahood: fix 18

--- kdramahood/kdramahood.js
+++ kdramahood/kdramahood.js
@@ -103,6 +103,9 @@ function extractStreamUrl(html) {
     const subtitlesMatch = html.match(/Download Subtitle :&nbsp  <a rel="nofollow" target="_blank" href="([^"]+)"/);
     const subtitles = subtitlesMatch ? subtitlesMatch[1].trim() : 'N/A';
 
+    console.log(stream);
+    console.log(subtitles);
+
     const result = {
         stream: stream,
         subtitles: "https://cca.megafiles.store/1b/bf/1bbfaac3e6a4f842f7a43dba86f34ee2/1bbfaac3e6a4f842f7a43dba86f34ee2.vtt",
