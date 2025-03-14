7c461f29cc958d3415132eee4c70dc03213b2db2
Ibrahim Sulejmenov <batonchik2004@gmail.com>
2025-03-14 20:08:26 +0100

    added frembed and fixed languages of modules

---
+++ frembed/frembed.js
@@ -0,0 +1,74 @@
+async function searchResults(keyword) {
+    try {
+        const encodedKeyword = encodeURIComponent(keyword);
+        const responseText = await fetch(`https://frembed.xyz/api/public/search?query=${encodedKeyword}`);
+        const data = JSON.parse(responseText);
+
+        const movieData = data.movies.map(movie => {
+            return {
+                title: movie.db-title || movie.original_title || movie.title || movie.name,
+                image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                href: `https://play.frembed.xyz/api/film.php?id=${movie.id}`
+            }
+        });
+
+        return JSON.stringify(movieData);
+    } catch (error) {
+        console.log('Fetch error in searchResults:', error);
+        return JSON.stringify([{ title: 'Error', image: '', href: '' }]);
+    }
+}
+
+async function extractDetails(url) {
+    try {
+        const match = url.match(/https:\/\/play\.frembed\.xyz\/api\/film\.php\?id=([^\/]+)/);
+        if (!match) throw new Error("Invalid URL format");
+        const movieId = match[1];
+        const responseText = await fetch(`https://frembed.xyz/api/public/movies/${movieId}`);
+        const data = JSON.parse(responseText);
+
+        const transformedResults = [{
+            description: data.overview || 'No description available',
+            // Movies use runtime (in minutes)
+            aliases: '',
+            airdate: ''
+        }];
+
+        return JSON.stringify(transformedResults);
+    } catch (error) {
+        console.log('Details error:', error);
+        return JSON.stringify([{
+            description: 'Error loading description',
+            aliases: 'Duration: Unknown',
+            airdate: 'Aired/Released: Unknown'
+        }]);
+    }
+}
+
+async function extractEpisodes(url) {
+    try {
+        const match = url.match(/https:\/\/play\.frembed\.xyz\/api\/film\.php\?id=([^\/]+)/);
+        if (!match) throw new Error("Invalid URL format");
+        const movieId = match[1];
+
+        return JSON.stringify([
+            { href: `https://play.frembed.xyz/api/film.php?id=${movieId}`, number: 1, title: "Full Movie" }
+        ]);
+    } catch (error) {
+        console.log('Fetch error in extractEpisodes:', error);
+        return JSON.stringify([]);
+    }    
+}
+
+async function extractStreamUrl(url) {
+    try {
+        const match = url.match(/https:\/\/play\.frembed\.xyz\/api\/film\.php\?id=([^\/]+)/);
+        if (!match) throw new Error("Invalid URL format");
+        const movieId = match[1];
+
+        return `https://streamtales.cc:8443/videos/${movieId}.mp4`;
+    } catch (error) {
+        console.log('Fetch error in extractStreamUrl:', error);
+        return null;
+    }
+}
