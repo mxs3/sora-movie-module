dad7cabb09123363432007799164bc4fc2e89970
Ibrahim Sulejmenov <batonchik2004@gmail.com>
2025-04-24 00:53:59 +0200

    binge subtitle fix 3

--- bingeflex/bingeflex.js
+++ bingeflex/bingeflex.js
@@ -502,8 +502,7 @@ async function extractStreamUrl(url) {
                 const subtitleTrackData = await subtitleTrackResponse.json();
 
                 const subtitleTrack = subtitleTrackData.find(track =>
-                    track.display.startsWith('English') && track.encoding === 'CP850'
-                    // && track.encoding === 'ASCII' || track.encoding === 'UTF-8'
+                    track.display.startsWith('English') && track.encoding === 'ASCII' || track.encoding === 'UTF-8'
                 );
 
                 subtitle = subtitleTrack ? subtitleTrack.url : '';
