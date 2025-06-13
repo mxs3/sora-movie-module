0e50ecd75abd7243351e95c9f7f5eeac9ec42c70
ibro <batonchik2004@gmail.com>
2025-06-13 21:13:47 +0200

    added vixsrc

--- dev/dev.js
+++ dev/dev.js
@@ -1,6 +1,6 @@
 async function searchResults(keyword) {
     const results = [];
-    const response = await fetchv2();
+    const response = await soraFetch();
     const html = await response.text();
 
     let match;
@@ -17,7 +17,7 @@ async function searchResults(keyword) {
 
 async function extractDetails(url) {
     const results = [];
-    const response = await fetchv2(url);
+    const response = await soraFetch(url);
     const html = await response.text();
 
     results.push({
@@ -31,7 +31,7 @@ async function extractDetails(url) {
 
 async function extractEpisodes(url) {
     const results = [];
-    const response = await fetchv2(url);
+    const response = await soraFetch(url);
     const html = await response.text();
 
         results.push({
@@ -43,8 +43,20 @@ async function extractEpisodes(url) {
 }
 
 async function extractStreamUrl(url) {
-    const response = await fetchv2(url);
+    const response = await soraFetch(url);
     const html = await response.text();
     
     return null;
-  }
+}
+
+async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
+    try {
+        return await fetchv2(url, options.headers ?? {}, options.method ?? 'GET', options.body ?? null);
+    } catch(e) {
+        try {
+            return await fetch(url, options);
+        } catch(error) {
+            return null;
+        }
+    }
+}
